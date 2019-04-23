50c48f6b56d6e4f8002908bb9ef630e39928767e
lelopez <ux.lelopez@gmail.com>
2019-04-23 10:53:55 -0500

    add header and default styling to app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header />
+    <div id="view-container">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
+<script>
+import Header from '@/components/layout/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
 <style lang="scss">
+html,
+body {
+  margin: 0;
+  max-width: 100% !important;
+  overflow-x: hidden !important;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+//this accounts for the header having a fixed position
+#view-container {
+  margin-top: 50px;
 }
 </style>
