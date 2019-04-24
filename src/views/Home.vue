ed364b8b70f7daa7edb15ac7a3c2d652b1a60fb7
lelopez <ux.lelopez@gmail.com>
2019-04-23 21:05:02 -0500

    add QR section to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Hero />
+    <ScanQR />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Hero from '@/components/Hero.vue'
+import ScanQR from '@/components/ScanQR.vue'
 
 export default {
   name: 'home',
   components: {
-    Hero
+    Hero,
+    ScanQR
   }
 }
 </script>
