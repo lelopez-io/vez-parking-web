7deace5ccc01cc0f1f956c82852d4a64e78e253b
lelopez <ux.lelopez@gmail.com>
2019-05-01 10:56:10 -0500

    update body copy

--- src/components/LearnMore.vue
+++ src/components/LearnMore.vue
@@ -2,13 +2,20 @@
   <div id="learn-more">
     <img src="@/assets/phone.jpg" />
     <div class="txt-ctn">
-      <h2>Infor</h2>
-      <p>This is information</p>
+      <h2>A Fleet of IoT Deviced</h2>
+      <p>
+        The VEZ project aims at fully leveraging the "Cloud" to enable quick and
+        easy parking. Our technology can be deployed both in garages and open
+        air lots. <a href="/about">Read More</a>
+      </p>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+$mint: #c1e7df;
+$dark-blue: #134d60;
+$slate-gray: #2c3e50;
 #learn-more {
   img {
     max-width: 100%;
@@ -19,24 +26,43 @@
     flex-direction: column;
     justify-content: center;
 
-    & > * {
-      max-width: 50%;
-    }
-
-    h2 {
-      padding-left: 30px;
-    }
-    p {
-      padding: 15px 30px;
-      font-size: 1.1em;
-      background-color: rgba(255, 255, 255, 0.5);
-    }
-
     background: url('../assets/phone.jpg');
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
     height: 500px;
+
+    & > * {
+      max-width: 60%;
+    }
+
+    h2,
+    p {
+      padding: 15px 30px;
+      background-color: rgba(255, 255, 255, 0.5);
+      margin: 0;
+    }
+
+    h2 {
+      padding-top: 30px;
+    }
+    p {
+      font-size: 1.1em;
+      padding-bottom: 30px;
+
+      a {
+        color: $dark-blue;
+        transition: padding 0.3s;
+
+        &:hover {
+          color: $mint;
+
+          background-color: $slate-gray;
+          padding: 0 1em;
+          transition: padding 0.3s;
+        }
+      }
+    }
   }
 }
 
